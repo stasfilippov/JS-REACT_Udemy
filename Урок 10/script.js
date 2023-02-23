5840a2ba0177adc39bb38d0adc2b71a34a60c269
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-02-23 21:50:13 +0300

    Работа с объектами и массивами

---
+++ Урок 10/script.js
@@ -0,0 +1,31 @@
+"use sctrict"; // работа в современном режиме
+
+/* const arr = ['a', 'b', 2]
+arr[10] = 'aaa';
+console.log(); */
+
+const afv = 'mother'; 
+const films = {
+    comedy: {
+        c: '1',
+        a: 'arab',
+        f: [1, afv]
+    }
+}
+console.log(films.comedy.f[1]);
+
+
+/* console.log(arr[1]);
+
+const arr0 = {
+    a: 1,
+    b:'b',
+    2:'c'
+}
+console.log(arr0.b);
+
+const f = 'f';
+
+arr0[f] = '1234';
+
+console.log(arr0['f']); */
