0090ed7f8a899f7ce964b227a7d820c12c61e105
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-02-23 22:50:58 +0300

    add: интерполяции

---
+++ Урок 12/script.js
@@ -0,0 +1,9 @@
+"use strict"; // работа в современном режиме
+
+//Интерполяция
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`); // использование интерполяции с помощью `` ${}
+
+const user = 'Stas';
+alert(`Привет,${user}`);
