ef566eb1def7d065b29207062aaa781503e39baf
stasfilippov <kron888999@gmail.com>
2023-03-06 17:23:40 +0300

    add: Циклы в цикле и метки

---
+++ урок 21. Циклы в цикле/script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/* for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j); 
+    }
+}
+ */
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+/* let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += '*';
+    }
+
+    result += '\n';
+}
+
+console.log(result); */
+
+// МЕТКИ 
+
+first: for (let i = 0; i < 3; i++) { // добавляем метку
+    console.log(`First level: ${i}`); // добавили интерполяцию
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`); 
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) {
+                continue first; // будет искать метку first для перехода к данному циклу
+            }
+            console.log(`Third level: ${k}`); 
+        }
+    }
+}
