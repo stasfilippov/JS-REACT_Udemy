adce5239c7aeac38bf4635513341f962dd248b28
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-03-14 20:56:44 +0300

    add: методы и свойство строк

---
+++ урок 26. Методы и свойства строк и чисел/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const str = 'string';
+
+console.log(str.toUpperCase());
+
+const fruits = 'some fruits';
+
+// console.log(fruits.slice(5, 11));
+
+console.log(fruits.substring(5, 11));
