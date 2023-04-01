9d2c30b428d4514383693ebc0f2175b0dcf6184f
stasfilippov <kron888999@gmail.com>
2023-04-01 22:03:55 +0300

    fix: определение длины объекта

---
+++ урок 30. Объект - методы/script.js
@@ -0,0 +1,30 @@
+
+'use strict';
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+
+console.log(Object.keys(options).length)
+
+let counter = 0;
+
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let j in options[key]) {
+            console.log(`Свойство ${j} имеет значение ${options[key][j]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
+}
+
+console.log(counter)
