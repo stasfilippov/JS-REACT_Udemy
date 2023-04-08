c2bb20d1b2f19fb83ae85ece3462bbc8c1093ae0
stasfilippov <kron888999@gmail.com>
2023-04-09 00:05:51 +0300

    add: примеры - обработчики событий

---
+++ урок 42. События и их обрпботчики (39/script.js
@@ -0,0 +1,44 @@
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+// btn.onclick = function() { // --- не используется в практике, при создании нескольких обработчиков для одного элемента - возможно переназначение, не удаляется 
+//     alert('click')
+// }
+
+
+// addEventListener -- дает назначать несколько обработчиков событий
+// btn.addEventListener('click', () => {
+//     alert('click')
+// })
+
+// btn.addEventListener('click', () => {
+//     alert('second click')
+// })
+
+// btn.addEventListener('mouseenter', () => {
+//     console.log('hover')
+// })
+
+// 
+let i = 0;
+
+const delElement = (event) => { 
+    console.log(event.currentTarget);
+    console.log(event.type)
+//     i++;
+//     if( i == 1) {
+//         btn.removeEventListener('click', delElement)
+//     }
+}
+
+btn.addEventListener('click', delElement); //происходит высплытие событий - когда обработчик событий срабатывает на самом вложенном элементе и поднимаясь вверх по иерархии
+overlay.addEventListener('click', delElement);
+
+
+const link = document.querySelector('a')
+
+link.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    console.log(event.target);
+}, {once: true})
