a2552caf84c83c3e898663aed94fd120f4d0b29f
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-04-12 23:39:26 +0300

    add: пример рекурсии

--- урок 44. Рекурсия (41)/script.js
+++ урок 44. Рекурсия (41)/script.js
@@ -25,3 +25,82 @@ console.log(pow (2, 4)); // 16
 
 
 
+let students = {
+    js: [{
+        name: 'John',
+        progress: 100
+    },{
+        name: 'Ivan',
+        progress: 60
+    }],
+    html: {
+        basic: [{
+            name: 'Petter',
+            progress: 20
+        },{
+            name: 'Ann',
+            progress: 18
+        }], 
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }],
+
+        semi
+    }
+};
+
+function getTotalProgressByIteretion (data) {
+    let total = 0;
+    let students = 0;
+
+    for (let course of Object.values(data)) {
+        if (Array.isArray(course)) {
+            students += course.length;
+            
+            for (let i = 0; i < course.length; i++) {
+                total += course[i].progress;
+            }
+        } else {
+            for (let subCourse of Object.values(course)) {
+                students += subCourse.length;
+
+                for (let i = 0; i < subCourse.length; i++) {
+                    total += subCourse[i].progress;
+                }
+            }
+        }
+    }
+
+    return total / students;
+}
+
+
+// console.log(getTotalProgressByIteretion(students))
+
+function getTotalProgressByRecursion (data) {
+    if (Array.isArray(data)) {
+        let total = 0;
+        
+        for (let i = 0; i < data.length; i++) {
+            total += data[i].progress;
+        }
+
+        return [total, data.length]
+    } else {
+        let total = [0, 0];
+
+        for (let subData of Object.values(data)) {
+            const subDataArr = getTotalProgressByRecursion (subData);
+
+            total[0] += subDataArr [0];
+            total[1] += subDataArr [1];
+            
+        }
+        return total;
+    }
+}
+
+const result = getTotalProgressByRecursion(students)
+
+console.log(result[0] / result [1])
