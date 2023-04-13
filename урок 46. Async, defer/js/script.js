1821daeb751b95a0a5b99f28fcf09b8402c65298
stasfilippov <kron888999@gmail.com>
2023-04-13 21:29:52 +0300

    add: использование async defer

---
+++ урок 46. Async, defer/js/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const p = document.querySelectorAll('p');
+
+console.log(p)
+
+function loadScript (src) {
+    const script = document.createElement('script');
+
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
+
+loadScript('js/test.js')
+
+loadScript('js/somejs.js')
