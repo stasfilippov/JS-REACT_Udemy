67cdbb2e8838f32a79fd31a065557fe0a38afe7f
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-04-14 10:41:10 +0300

    add: живые коллекции

---
+++ урок 49. Живая коллекция/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const boxesQuery = document.querySelectorAll('.box')
+const boxesGet = document.getElementsByClassName('box');
+
+boxesQuery[0].remove();
+boxesGet[0].remove();
+
+
+console.log(boxesQuery);
+console.log(boxesGet);
