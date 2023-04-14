1fbb6ff1f1cb5d513db3524ff78ba35311712039
stasfilippov <kron888999@gmail.com>
2023-04-14 22:25:12 +0300

    add: применение classLIst

---
+++ Модуль 3/урок 1. ClassList и делегирование/script.js
@@ -0,0 +1,13 @@
+const btns = document.querySelectorAll('button');
+
+// console.log(btns[0].classList.length);
+
+// console.log(btns[0].classList.item(0));
+
+console.log(btns[0].classList.add('red'));
+
+// console.log(btns[0].classList.remove('blue'));
+
+console.log(btns[0].classList.toggle('blue'));
+
+
